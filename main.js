32d4f2b0a9320e4e2460684362737dc7dd41257f
eah645 <92736056+eah645@users.noreply.github.com>
2022-06-14 21:44:37 -0400

    base upload

---
+++ main.js
@@ -0,0 +1,11 @@
+/*idk why this isn't working I copied the code from the button just put it in a function*/
+/*it only works in the html page not on here idk if it's referenceed wrong or something*/
+function punchline() {
+  document.getElementById("edit").innerHTML = "Sundae School";
+}
+
+function nextJoke() {
+  document.getElementById("edit").innerHTML =
+    "What did the ocean say to the beach?";
+  document.getElementById("nextButton").innerHTML = "What?";
+}
